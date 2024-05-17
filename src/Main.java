bb93d99f4621d0d563056d11a2d00634a37316c0
Leysen95 <mullayanova95@bk.ru>
2024-05-17 19:39:50 +0500

    Task 1 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        int age = 25;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", он не достиг совершеннолетия, нужно немного подождать");
+        }
     }
 }
